37767e1a05943a7cf4288cabfbae88db9a917d4c
raideer <idmania1@gmail.com>
2015-12-26 17:23:36 +0200

    Finished Channels resource

--- src/Resources/Channels.php
+++ src/Resources/Channels.php
@@ -17,22 +17,34 @@ class Channels extends Resource{
     return $this->wrapper->request("GET","channels/$name");
   }
 
-  public function getEditors(){
+  public function getEditors($channel){
     $this->wrapper->checkScope("channel_read", true);
 
-    return $this->wrapper->request("GET","channels/$name/editors", [], true);
+    return $this->wrapper->request("GET","channels/$channel/editors", [], true);
   }
 
   public function updateChannel($channel, $params){
     $this->wrapper->checkScope("channel_editor", true);
 
-    return $this->wrapper->request("PUT", "channels/$channel/", ['form_params' => $params], true);
+    return $this->wrapper->request("PUT", "channels/$channel/", ['form_params' => ['channel' => $params]], true);
   }
 
   public function resetStreamKey($channel){
     $this->wrapper->checkScope("channel_stream", true);
 
     return $this->wrapper->request("DELETE", "channels/$channel/stream_key", [], true);
+  }
+
+  public function startCommercial($channel, $length = 30){
+    $this->wrapper->checkScope("channel_commercial", true);
+
+    $values = [
+      "length" => [30,60,90,120,150,180]
+    ];
+
+    $resolved = $this->resolveOptions(['length' => $length], [], ['length'], $values);
+
+    return $this->wrapper->request("POST", "channels/$channel/commercial", ['form_params' => $resolved], true);
   }
 
   public function getVideos($name){
