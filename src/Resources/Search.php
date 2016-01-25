0863b3fb1f5bd84e5fd50057426e41e432e5ad58
raideer <idmania1@gmail.com>
2016-01-25 14:53:30 +0200

    Finishing Search resource

--- src/Resources/Search.php
+++ src/Resources/Search.php
@@ -1,45 +1,82 @@
 <?php
 namespace Raideer\TwitchApi\Resources;
 
+/**
+ * Search for channels, streams or games with queries.
+ */
 class Search extends Resource{
 
+  /**
+   * Returns the resource name
+   * @return string
+   */
   public function getName(){
     return "search";
   }
 
-  public function searchChannels($query, $options = []){
+  /**
+   * Returns a list of channel objects matching the search query.
+   *
+   * Learn more:
+   * https://github.com/justintv/Twitch-API/blob/master/v3_resources/search.md#get-searchchannels
+   *
+   * @param  string $query   Search query
+   * @param  array $params  Optional parameters
+   * @return array
+   */
+  public function searchChannels($query, $params = []){
 
     $defaults = [
-      "query" => $query,
+      "query" => urlencode($query),
       "limit" => 25,
       "offset" => 0
     ];
 
-    return $this->wrapper->request("GET","search/channels", ['query' => $this->resolveOptions($options, $defaults)]);
+    return $this->wrapper->request("GET","search/channels", ['query' => $this->resolveOptions($params, $defaults)]);
   }
 
-  public function searchStreams($query, $options = []){
+  /**
+   * Returns a list of stream objects matching the search query
+   *
+   * Learn more:
+   * https://github.com/justintv/Twitch-API/blob/master/v3_resources/search.md#get-searchstreams
+   *
+   * @param  string $query   Search query
+   * @param  array $params Optional params
+   * @return array
+   */
+  public function searchStreams($query, $params = []){
 
     $defaults = [
-      "query" => $query,
+      "query" => urlencode($query),
       "limit" => 25,
       "offset" => 0,
       "hls" => null
     ];
 
-    return $this->wrapper->request("GET","search/streams", ['query' => $this->resolveOptions($options, $defaults)]);
+    return $this->wrapper->request("GET","search/streams", ['query' => $this->resolveOptions($params, $defaults)]);
   }
 
-  public function searchGames($query, $options = []){
+  /**
+   * Returns a list of game objects matching the search query
+   *
+   * Learn more:
+   * https://github.com/justintv/Twitch-API/blob/master/v3_resources/search.md#get-searchgames
+   *
+   * @param  string $query   Search query
+   * @param  array $params  Optional params
+   * @return array
+   */
+  public function searchGames($query, $params = []){
 
     $defaults = [
-      "query" => $query,
+      "query" => urlencode($query),
       "limit" => 25,
       "type" => "suggest",
       "live" => false
     ];
 
-    return $this->wrapper-request("GET","search/games", ['query' => $this->resolveOptions($options, $defaults)]);
+    return $this->wrapper-request("GET","search/games", ['query' => $this->resolveOptions($params, $defaults)]);
   }
 
 }
