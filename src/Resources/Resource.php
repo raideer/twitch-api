93d783c4bc2fc652110a990df06b888c83d69c69
raideer <idmania1@gmail.com>
2016-01-25 14:47:16 +0200

    Finishing abstract Resource

--- src/Resources/Resource.php
+++ src/Resources/Resource.php
@@ -4,10 +4,19 @@ namespace Raideer\TwitchApi\Resources;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Raideer\TwitchApi\Wrapper;
 
+/**
+ * This is the parent class of all Resources
+ */
 abstract class Resource{
 
   protected $wrapper;
 
+  /**
+   * Takes Wrapper instance as its only parameter
+   * Automatically self-registers the resource
+   *
+   * @param Wrapper $wrapper
+   */
   public function __construct(Wrapper $wrapper){
 
     $this->wrapper = $wrapper;
@@ -15,8 +24,23 @@ abstract class Resource{
 
   }
 
+  /**
+   * Required by all resources
+   * Used when registering the resource
+   *
+   * @return string
+   */
   abstract function getName();
 
+  /**
+   * Helper function for resolving parameters
+   *
+   * @param  array $options      Passed params
+   * @param  array $defaults     Default values
+   * @param  array $required     Required fields
+   * @param  array $allowedTypes Allowed field values
+   * @return array
+   */
   public function resolveOptions($options, $defaults, $required = [], $allowedTypes = []){
 
     $resolver = new OptionsResolver();
