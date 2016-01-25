35542e56d0b349fcca5384ea566e57203e288dd6
raideer <idmania1@gmail.com>
2016-01-25 16:51:50 +0200

    Created a temporary throttle class

---
+++ src/Throttle.php
@@ -0,0 +1,52 @@
+<?php
+namespace Raideer\TwitchApi;
+
+class Throttle{
+
+  protected $firstCall;
+  protected $lastCall;
+  protected $waitBeforeNext;
+  protected $limit;
+  protected $limitTimeframe;
+  protected $calls;
+
+  public function __construct($wait = 0){
+    $this->waitBeforeNext = $wait;
+    $this->limit = 0;
+  }
+
+  public function throttle(){
+    $mkTime = microtime();
+
+    if(!$this->lastCall){
+      $this->lastCall = $mkTime;
+      $this->firstCall = $mkTime;
+      return;
+    }
+
+    // if($this->calls >= $this->limit){
+    //   $blockTill = $this->firstCall + $this->limitTimeframe;
+    //   if($blockTill > $mkTime){
+    //     usleep($blockTill - $mkTime);
+    //     $this->firstCall = microtime();
+    //   }
+    // }
+
+    $dif = $mkTime - $this->lastCall;
+    if($dif < $this->waitBeforeNext){
+      usleep($this->waitBeforeNext - $dif);
+    }
+
+    $this->lastCall = microtime();
+  }
+
+  public function setInterval($ms){
+    $this->waitBeforeNext = $ms * 1000;
+  }
+
+  // public function setLimit($limit, $time){
+  //   $this->limit = $limit;
+  //   $this->limitTimeframe = $time;
+  // }
+
+}
