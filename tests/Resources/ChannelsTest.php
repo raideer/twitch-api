577cfaba785f27fe20c4ce0017996c99c068e1e7
raideer <idmania1@gmail.com>
2016-02-13 18:21:03 +0200

    Added more tests

--- tests/Resources/ChannelsTest.php
+++ tests/Resources/ChannelsTest.php
@@ -21,4 +21,60 @@ class ChannelsTest extends PHPUnit_Framework_TestCase
 
         $this->assertSame('channels', $resource->getName());
     }
+
+    public function test_getChannel()
+    {
+        $this->wrapper->shouldReceive('checkScope')->with('channel_read');
+        $this->wrapper->shouldReceive('request')->withArgs([
+          'GET',
+          'channel',
+          [],
+          true,
+        ]);
+
+        $resource = $this->resource;
+        $resource->getChannel();
+    }
+
+    public function test_getChannelName()
+    {
+        $this->wrapper->shouldReceive('checkScope')->with('channel_read');
+        $this->wrapper->shouldReceive('request')->withArgs([
+          'GET',
+          'channel/testchannel',
+          [],
+          true,
+        ]);
+
+        $resource = $this->resource;
+        $resource->getChannel('testchannel');
+    }
+
+    public function test_getEditors()
+    {
+        $this->wrapper->shouldReceive('checkScope')->with('channel_read');
+        $this->wrapper->shouldReceive('request')->withArgs([
+          'GET',
+          'channels/testchannel/editors',
+          [],
+          true,
+        ]);
+
+        $resource = $this->resource;
+        $resource->getEditors('testchannel');
+    }
+
+    public function test_updateChannel()
+    {
+        $this->wrapper->shouldReceive('checkScope')->with('channel_editor');
+        $this->wrapper->shouldReceive('request')->withArgs([
+          'PUT',
+          'channels/testchannel',
+          ['form_params' => ['status' => 'hello', 'game' => 'Rust']],
+          true,
+        ]);
+
+        $resource = $this->resource;
+        $resource->updateChannel('testchannel', ['status' => 'hello', 'game' => 'Rust']);
+    }
 }
